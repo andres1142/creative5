2cd1cb35f5c0b05376faacc624894d37a1d2c727
andres <afmo2@byu.edu>
2022-12-08 21:06:51 -0700

    Updated

--- front-end/src/components/Footer.js
+++ front-end/src/components/Footer.js
@@ -4,7 +4,7 @@ import gitLogo from './images/GitHub-Mark.png'
 function Footer() {
     return (
         <footer>
-            <a href={'https://github.com/andres1142/creative4.git'}>
+            <a href={'https://github.com/andres1142/creative5.git'}>
                 <img src={gitLogo} width={'100px'} alt="Logo" />
             </a>
         </footer>
